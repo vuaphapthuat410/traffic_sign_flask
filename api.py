30501f390ba0883791ada688f8238fe0289db45f
vuaphapthuat410 <vuaphapthuat410@gmail.com>
2021-05-24 13:09:51 +0700

    add runtime.txt

--- api.py
+++ api.py
@@ -121,7 +121,3 @@ def classify():
             }), 404
     except Exception as e:
         return repr(e), 500
-
-
-if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
